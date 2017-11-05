8a83070c36dc5232f51f2b5d4a909ade5a10e707
hzj5158888 <www1693303798@163.com>
2017-11-05 18:56:37 +0800

    up

--- libshims/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-LOCAL_SRC_FILES := atomic.cpp
-LOCAL_SHARED_LIBRARIES := libcutils libutils
-LOCAL_MODULE := libshims_atomic
-LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := liblog
-
-include $(BUILD_SHARED_LIBRARY)
